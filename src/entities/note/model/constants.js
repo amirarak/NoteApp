1434c54c2e17cae2fc4a90f99888298ddbaeb459
Amira <amirarakieva@gmail.com>
2024-12-20 00:10:52 +0600

    Refactor: replace 'todo' entity with new entity

---
+++ src/entities/note/model/constants.js
@@ -0,0 +1,6 @@
+export const NOTE_ERRORS = {
+    FETCH_ERROR: 'Failed to fetch notes',
+    CREATE_ERROR: 'Failed to create note',
+    UPDATE_ERROR: 'Failed to update note',
+    DELETE_ERROR: 'Failed to delete note',
+  }
